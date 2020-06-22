1074ec8ebaa2ff7438c3432986eeed383f92915f
anastasia21112 <anad221122@gmail.com>
2020-06-21 18:02:18 -0600

    starting exercises

---
+++ ExampleApp/app/src/main/java/com/myappcompany/rob/exampleapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.myappcompany.rob.exampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void loginFunction(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Info", "Login Clicked");
+
+        Log.i("Values", usernameEditText.getText().toString());
+        Log.i("Values", passwordEditText.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
